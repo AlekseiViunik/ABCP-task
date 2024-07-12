e5298646298068d39d6d584d18fd23b9b6406099
Aleksei Viunik <a79643739989@yandex.ru>
2024-07-13 00:21:22 +0200

    Move ReferencesOperation to a separate PHP file

--- php/v2/others.php
+++ php/v2/others.php
@@ -2,16 +2,6 @@
 
 namespace NW\WebService\References\Operations\Notification;
 
-abstract class ReferencesOperation
-{
-    abstract public function doOperation(): array;
-
-    public function getRequest($pName)
-    {
-        return $_REQUEST[$pName];
-    }
-}
-
 function getResellerEmailFrom(): string
 {
     return 'contractor@example.com';
