d063eac803173f8aea0fa956314e00d65b34f7b5
Aleksei Viunik <a79643739989@yandex.ru>
2024-07-13 00:25:39 +0200

    Add PHPDoc, rename variable, add return type declaration

--- php/v2/ReferencesOperation.php
+++ php/v2/ReferencesOperation.php
@@ -2,12 +2,26 @@
 
 namespace NW\WebService\References\Operations\Notification;
 
+/**
+ * Class ReferencesOperation
+ */
 abstract class ReferencesOperation
 {
+    /**
+     * Execute the operation.
+     *
+     * @return array
+     */
     abstract public function doOperation(): array;
 
-    public function getRequest($pName)
+    /**
+     * Get a request parameter by name.
+     *
+     * @param string $parameterName
+     * @return mixed
+     */
+    public function getRequest($parameterName): mixed
     {
-        return $_REQUEST[$pName];
+        return $_REQUEST[$parameterName];
     }
 }
