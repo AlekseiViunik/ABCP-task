71eef086ff4693e27ecd211a43b3e42106d9d318
Aleksei Viunik <a79643739989@yandex.ru>
2024-07-13 00:32:02 +0200

    Move NotificationEvents to a separate PHP file

--- php/v2/others.php
+++ php/v2/others.php
@@ -12,9 +12,3 @@ function getEmailsByPermit($resellerId, $event): array
     // fakes the method
     return ['someemeil@example.com', 'someemeil2@example.com'];
 }
-
-class NotificationEvents
-{
-    public const CHANGE_RETURN_STATUS = 'changeReturnStatus';
-    const NEW_RETURN_STATUS    = 'newReturnStatus';
-}
