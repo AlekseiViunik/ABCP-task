011940d1a09414e93aab8e23d73c0b007ccc5f53
Aleksei Viunik <a79643739989@yandex.ru>
2024-07-13 13:17:51 +0200

    add a sendMessage function to EMailHelper. It will serve instead of MessagesClient::sendMessage

--- php/v2/EmailHelper.php
+++ php/v2/EmailHelper.php
@@ -30,4 +30,24 @@ class EmailHelper
         // fakes the method
         return ['someemeil@example.com', 'someemeil2@example.com'];
     }
+
+    /**
+     * Sends a message using the specified parameters.
+     *
+     * @param array $message The message details, including emailFrom, emailTo, subject, and body.
+     * @param int $resellerId The ID of the reseller.
+     * @param string $status The status of the notification event.
+     * @param int|null $clientId The ID of the client (optional).
+     * @param int|null $notificationType The type of notification (optional).
+     * @return void
+     */
+    public static function sendMessage(
+        array $message,
+        int $resellerId,
+        string $status,
+        int $clientId = null,
+        int $notificationType = null
+    ): void {
+        // add logic here
+    }
 }
